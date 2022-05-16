132d57dc2fe9f16eb6f090ade22a1cd7165420c2
Michael Barreau <davidb153@gmail.com>
2022-05-16 18:24:43 -0500

    Added tag draft

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
 import { CategoryList } from "./Categories/CategoryList"
+import {TagForm} from "./tag/TagForm" 
 
 export const ApplicationViews = () => {
-  // return (
-  //   <h1 >Welcome to Rare Publishing</h1>
-  // )
 
   return (
     <>
       <Switch>
+      <h1 >Welcome to Rare Publishing</h1>
         {/* <Route exact path="/" element={<Home />} /> */}
-        <Route path="/category" component={CategoryList} />
+        <Route path="/Categories" component={CategoryList} />
+      </Switch>
+
+      <Switch>
+        <Route path="/tag"  component={TagForm} /> 
       </Switch>
     </>
   )
